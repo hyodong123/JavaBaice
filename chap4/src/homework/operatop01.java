97dd3f56e59afe9100b735f297fb05fe0ad40d12
hyodong123 <162409720+hyodong123@users.noreply.github.com>
2024-03-11 16:35:46 +0900

    Add files via upload

---
+++ chap4/src/homework/operatop01.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class operatop01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10, y = 20;
+		System.out.println("x + y =" + (x+y));
+		System.out.println("x - y =" + (x-y));
+		System.out.println("x * y =" + (x*y));
+		System.out.println("x / y =" + (x/y));
+		System.out.println("x % y =" + (x%y));
+	}
+
+}
